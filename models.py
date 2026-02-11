d90681f697ffbf18b42065025aa12559857c3e09
rziyoda2197 <rziyoda2197@gmail.com>
2026-02-11 15:02:19 +0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Client(Base):
+    __tablename__ = "clients"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    phone = Column(String)
+    note = Column(String)
